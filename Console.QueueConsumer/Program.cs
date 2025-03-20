31ab9194ccde53779d3176ce30cc5ca3d7e03ef5
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2025-03-20 10:16:58 +0200

    Implemented Queue message reader

---
+++ Console.QueueConsumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets<Program>();
+
+IConfigurationRoot configuration = builder.Build();
+
+string? connectionString = configuration["AzureStorageAccountConnection"];
+string? queueName = configuration["AzureQueueName"];
+
+QueueClient queue = new QueueClient(connectionString, queueName);
+if (await queue.ExistsAsync())
+{
+    await ProcessMessagesAsync();
+}
+
+async Task ProcessMessagesAsync()
+{
+    QueueMessage[] messages = await queue.ReceiveMessagesAsync(maxMessages: 10);
+
+    foreach (QueueMessage message in messages)
+    {
+        Console.WriteLine($"Message: {message.MessageText}");
+    }
+}
